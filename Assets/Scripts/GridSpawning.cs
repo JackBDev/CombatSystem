bcbecec5001bfe657be9c5a86283c5a7fbfc76df
Jack Bushnell <jonathan@jonathanbushnell.tech>
2021-05-17 21:06:01 -0400

    still working on the spawn animation

--- Assets/Scripts/GridSpawning.cs
+++ Assets/Scripts/GridSpawning.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class GridSpawning : MonoBehaviour
 {
+    
+
+    [Tooltip("Height should be 1 less than the width if you want each side to be square.")]
     public int gridHeight = 3;
+    [Tooltip("This should be one more than the height because the first block in a row is not counted when spawning.")]
     public int gridWidth = 4;
+    [Tooltip("Length of time between triggering each spawn animation. Try to keep under half a second.")]
     public float tileSpawnDelay;
+    [Tooltip("The prefap for the blocks that make up the battlefield.")]
     public GameObject tilePrefab;
 
-    [HideInInspector]
-    public List<GameObject[]> friendlyTiles = new List<GameObject[]>();
-    [HideInInspector]
-    public List<GameObject[]> enemyTiles = new List<GameObject[]>();
+    private List<GameObject> friendlyTiles = new List<GameObject>();
+    private List<GameObject> enemyTiles = new List<GameObject>();
 
-    private GameObject[] row;
-    private UsefulMethods functions;
+    //private GameObject[] row;
+    //private int[] rowTileTracker;
     private float tileWidth;
 
     void Awake()
     {
         tileWidth = UsefulMethods.GetObjectSize(tilePrefab.transform.GetChild(0).gameObject).x;
-        row = new GameObject[gridWidth];
+        //row = new GameObject[gridWidth];
+        //rowTileTracker = new int[gridWidth];
+
+        //for(int i = rowTileTracker.Length - 1; i >= 0; i--)
+        //{
+        //    rowTileTracker[i] = 0;
+        //}
 
         SpawnBattlefieldGrid();
     }
@@ -36,17 +46,17 @@ public class GridSpawning : MonoBehaviour
         {
             newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
             newTile.transform.parent = transform;
-            row[0] = newTile;
+            newTile.transform.name = a.ToString() + 0.ToString();
+            friendlyTiles.Add(newTile);
 
             for (int b = 1; b <= gridHeight; b++)
             {
                 spawnPosition = spawnPosition + new Vector3(-tileWidth, 0f, 0f);
                 newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
+                newTile.transform.name = a.ToString() + b.ToString();
                 newTile.transform.parent = transform;
-                row[b] = newTile;
+                friendlyTiles.Add(newTile);
             }
-
-            friendlyTiles.Add(row);
             spawnPosition = transform.position + new Vector3(0f, 0f, tileWidth * -a);
         }
 
@@ -57,16 +67,17 @@ public class GridSpawning : MonoBehaviour
         {
             newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
             newTile.transform.parent = transform;
-            row[0] = newTile;
+            newTile.transform.name = a.ToString() + 0.ToString();
+            enemyTiles.Add(newTile);
 
             for (int b = 1; b <= gridHeight; b++)
             {
                 spawnPosition = spawnPosition + new Vector3(tileWidth, 0f, 0f);
                 newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
+                newTile.transform.name = a.ToString() + b.ToString();
                 newTile.transform.parent = transform;
-                row[b] = newTile;
+                enemyTiles.Add(newTile);
             }
-            enemyTiles.Add(row);
             spawnPosition = transform.position + new Vector3(tileWidth, 0f, tileWidth * -a);
         }
 
@@ -75,11 +86,19 @@ public class GridSpawning : MonoBehaviour
 
     private IEnumerator SpawnAnimationDelay()
     {
-        for(int i = 1; i <= friendlyTiles.Count; i++)
+        Debug.Log("Count: " + friendlyTiles.Count);
+
+        for (int i = 0; i <= friendlyTiles.Count - 1; i++)
         {
+            Debug.Log("I: " + i);
 
-        }
+            
 
-        yield return new WaitForSeconds(tileSpawnDelay);
+            Transform block = friendlyTiles[j].transform.GetChild(0);
+
+            block.GetComponent<Animator>().SetBool("Spawn", true);
+
+            yield return new WaitForSeconds(tileSpawnDelay);
+        }  
     }
 }
