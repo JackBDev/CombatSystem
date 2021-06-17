3a720aceefe0fe6b66ae9c9ee6a67a1fbe271087
Jack Bushnell <jonathan@jonathanbushnell.tech>
2021-06-17 19:18:54 -0400

    adding scripts for combat functionality

---
+++ Assets/Scripts/CombatInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatInfo : MonoBehaviour
+{
+    public GameObject playerReference;
+    public List<GameObject> enemyList = new List<GameObject>();
+}
