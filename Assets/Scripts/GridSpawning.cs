737195a791e20b429680ddb170fdc7239dc6491a
Jack Bushnell <jonathan@jonathanbushnell.tech>
2021-06-17 18:23:10 -0400

    Made grid spawning more compact, reduced code by 20 lines and make it much more readable.

--- Assets/Scripts/GridSpawning.cs
+++ Assets/Scripts/GridSpawning.cs
@@ -7,7 +7,7 @@ public class GridSpawning : MonoBehaviour
     [Tooltip("Height should be 1 less than the width if you want each side to be square.")]
     public byte gridHeight = 3;
     [Tooltip("This should be one more than the height because the first block in a row is not counted when spawning.")]
-    public byte gridWidth = 4;
+    public byte gridWidth = 5;
     [Tooltip("Length of time between triggering each spawn animation. Try to keep under half a second.")]
     public float tileSpawnDelay;
     [Tooltip("The prefap for the blocks that make up the battlefield.")]
@@ -21,7 +21,7 @@ public class GridSpawning : MonoBehaviour
     {
         tileWidth = UsefulMethods.GetObjectSize(tilePrefab.transform.GetChild(0).gameObject).x;
 
-        for(byte i = 0; i < gridWidth; i++)
+        for (byte i = 0; i < gridWidth; i++)
         {
             friendlyTiles.Add(new GameObject[gridWidth]);
             enemyTiles.Add(new GameObject[gridWidth]);
@@ -40,65 +40,46 @@ public class GridSpawning : MonoBehaviour
 
     IEnumerator SpawnAnimationDelay()
     {
-        Vector3 spawnPosition = transform.position;
-        GameObject newTile;
+        Vector3 spawnPositionPlayerTiles = transform.position;
+        Vector3 spawnPositionEnemyTiles = transform.position;
+        
         WaitForSecondsRealtime wait = new WaitForSecondsRealtime(tileSpawnDelay);
 
         //Player's Grid
         for (byte a = 0; a < gridWidth; a++)
         {
-            spawnPosition = transform.position + new Vector3(0f, 0f, tileWidth * -a);
+            spawnPositionPlayerTiles = transform.position + new Vector3(0f, 0f, tileWidth * -a);
 
-            newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
+            SpawnTile(spawnPositionPlayerTiles, friendlyTiles, a, 0);
 
-            newTile.transform.parent = transform;
-            newTile.transform.name = a.ToString() + 0.ToString();
-            newTile.transform.GetChild(0).GetComponent<Animator>().SetBool("Spawn", true);
+            spawnPositionEnemyTiles = transform.position + new Vector3(tileWidth, 0f, tileWidth * -a);
 
-            friendlyTiles[a][0] = newTile;
+            SpawnTile(spawnPositionEnemyTiles, enemyTiles, a, 0);
 
             for (byte b = 1; b <= gridHeight; b++)
             {
                 yield return wait;
-                spawnPosition = spawnPosition + new Vector3(-tileWidth, 0f, 0f);
 
-                newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
-                
-                newTile.transform.parent = transform;
-                newTile.transform.name = a.ToString() + b.ToString();
-                newTile.transform.GetChild(0).GetComponent<Animator>().SetBool("Spawn", true);
+                spawnPositionPlayerTiles = spawnPositionPlayerTiles + new Vector3(-tileWidth, 0f, 0f);
 
-                friendlyTiles[a][b] = newTile;
+                SpawnTile(spawnPositionPlayerTiles, friendlyTiles, a, b);
+
+                spawnPositionEnemyTiles = spawnPositionEnemyTiles + new Vector3(tileWidth, 0f, 0f);
+
+                SpawnTile(spawnPositionEnemyTiles, enemyTiles, a, b);
             } 
         }
+    }
 
-        //Enemy's Grid
-        for (byte a = 0; a < gridWidth; a++)
-        {
-            spawnPosition = transform.position + new Vector3(tileWidth, 0f, tileWidth * -a);
+    public void SpawnTile(Vector3 spawnPosition, List<GameObject[]> list, int indexX, int indexY)
+    {
+        GameObject newTile;
 
-            newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
+        newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
+        newTile.transform.parent = transform;
+        newTile.transform.name = indexX.ToString() + indexY.ToString();
+        newTile.transform.GetChild(0).GetComponent<Animator>().SetBool("Spawn", true);
 
-            newTile.transform.parent = transform;
-            newTile.transform.name = a.ToString() + 0.ToString();
-            newTile.transform.GetChild(0).GetComponent<Animator>().SetBool("Spawn", true);
-
-            enemyTiles[a][0] = newTile;
-
-            for (byte b = 1; b <= gridHeight; b++)
-            {
-                yield return wait;
-                spawnPosition = spawnPosition + new Vector3(tileWidth, 0f, 0f);
-
-                newTile = Instantiate(tilePrefab, spawnPosition, Quaternion.identity);
-                
-                newTile.transform.parent = transform;
-                newTile.transform.name = a.ToString() + b.ToString();
-                newTile.transform.GetChild(0).GetComponent<Animator>().SetBool("Spawn", true);
-
-                enemyTiles[a][b] = newTile;
-            }
-            
-        }
+        friendlyTiles[indexX][indexY] = newTile;
     }
 }
